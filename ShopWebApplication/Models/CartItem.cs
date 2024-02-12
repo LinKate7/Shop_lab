defe02a01e7859cc7da5402074a9dfc33100965f
Kateryna Lahoda <katelagoda7@gmail.com>
2024-02-12 21:00:17 +0200

    First commit

---
+++ ShopWebApplication/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopWebApplication.Models;
+
+public partial class CartItem
+{
+    public int CartItemId { get; set; }
+
+    public int CartId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int CartItemQuantity { get; set; }
+
+    public virtual Cart Cart { get; set; } = null!;
+
+    public virtual Product Product { get; set; } = null!;
+}
---
+++ ShopWebApplication/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopWebApplication.Models;
+
+public partial class Order
+{
+    public int OrderId { get; set; }
+
+    public int UserId { get; set; }
+
+    public DateTime OrderDate { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int StatusId { get; set; }
+
+    public int ClientInfoId { get; set; }
+
+    public virtual ClientInfo ClientInfo { get; set; } = null!;
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    public virtual Status Status { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
